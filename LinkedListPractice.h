676aa3bbd472913a40dcbb7e742478d3db111319
Krys <21113012+Krystade@users.noreply.github.com>
2019-11-23 16:34:42 -0800

    Update LinkedListPractice.h
    added the deleteItem function and did some bug fixes

--- LinkedListPractice.h
+++ LinkedListPractice.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 class Item;
+class Data;
 
 template <class T> class Header{
 public:
@@ -15,11 +16,12 @@ public:
 	Header(){first = NULL, last = NULL, size = 0;};
 	Header(T * f, T * l, int s);
 	Header(const T& oldHeader){first = oldHeader.first, last = oldHeader.last, size = oldHeader.size;};
-	~Header(){};
+	~Header(){cout << "Deleting Header object\n";};
 	void print(){cout << "\tHeader" << "\naddress: " << this << "\nfirst: " << first << "\nlast: " << last << "\nsize: " << size << endl << endl;}
 	void appendItem();
 	void insertItem(int pos);
 	void printItems();
+	void deleteItem(int pos);
 };
 
 class Item{
@@ -27,10 +29,21 @@ public:
 	Item * next;
 	Item * prev;
 	Header<Item> * header;
-	Item(){next = NULL, prev = NULL, header = NULL;};
-	Item(Item *n, Item *p, Header<Item> *h);
-	~Item(){};
+	Data *data;
+	Item(){next = NULL, prev = NULL, header = NULL, data = NULL;};
+	Item(Item *n, Item *p, Header<Item> *h, Data *data);
+	~Item(){cout << "Deleting Item object\n";};
 	void print(){cout << "\tItem" << "\naddress: " << this << "\nprev: " << prev << "\nnext: " << next << "\nheader: " << header << endl << endl;}
+};
+
+class Data{
+public:
+	int x;
+	int y;
+	int z;
+	Data(){x = 0, y = 0, z = 0;};
+	Data(int x, int y, int z);
+	~Data(){cout << "Deleting Data object\n";};
 };
 
 #endif /* LINKEDLISTS2_H_ */
